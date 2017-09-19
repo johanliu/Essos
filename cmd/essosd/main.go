59dc53aad8c5c84ced757dca27da7195f3ad3da7
johanliu <akira.lau@hotmail.com>
2017-09-19 19:56:16 +0800

    bug fix

--- cmd/essosd/main.go
+++ cmd/essosd/main.go
@@ -1,4 +1,4 @@
-package essosd
+package main
 
 import (
 	"context"
@@ -11,14 +11,15 @@ import (
 
 	"github.com/johanliu/Vidar/constant"
 	"github.com/johanliu/essos"
+	"github.com/johanliu/essos/cmd"
 	"github.com/johanliu/mlog"
 	"github.com/johanliu/vidar"
 	"github.com/johanliu/vidar/middlewares"
 )
 
 const (
-	pluginDir  = "./components"
 	entryPoint = "Entry"
+	configPath = "/etc/essos.conf"
 )
 
 type compon struct {
@@ -30,28 +31,29 @@ type essosd struct {
 	components map[string]*compon
 }
 
-func (e *essosd) loadPlugins() error {
+func (e *essosd) loadPlugins(pluginDir string, l cmd.LibraryInfo) error {
 	if _, err := os.Stat(pluginDir); err != nil {
 		return err
 	}
 
-	ps, err := listFiles(pluginDir, `*.so`)
+	ps, err := listFiles(pluginDir, `.so`)
 	if err != nil {
 		return err
 	}
+
+	fmt.Println(ps)
 
 	for _, p := range ps {
 		//Open library in PLUGIN_DIR read from configuration file
 		lib, err := plugin.Open(path.Join(pluginDir, p.Name()))
 		if err != nil {
-			fmt.Printf("failed to open plugin %s: %v\n", p.Name(), err)
+			e.log.Info("failed to open plugin %s: %v\n", p.Name(), err)
 			continue
 		}
-
 		//Lookup Component variable in plugin which is entry point
 		symbol, err := lib.Lookup(entryPoint)
 		if err != nil {
-			fmt.Printf("plugin %s does not export symbol \"%s\"\n",
+			e.log.Info("plugin %s does not export symbol \"%s\"\n",
 				p.Name(), entryPoint)
 			continue
 		}
@@ -59,15 +61,20 @@ func (e *essosd) loadPlugins() error {
 		//Validate the symbol loaded from plugin
 		component, ok := symbol.(essos.Component)
 		if !ok {
-			fmt.Printf("Symbol %s (from %s) does not implement Component interface\n",
+			e.log.Info("Symbol %s (from %s) does not implement Component interface\n",
 				entryPoint, p.Name())
+			continue
+		}
+
+		if err := component.Init(); err != nil {
+			e.log.Info("%s initialization failed: %v\n", p.Name(), err)
 			continue
 		}
 
 		//Get the operations supported from plugin
 		ops := component.Discover()
 		if ops == nil {
-			fmt.Printf("No operations in %s\n", p.Name())
+			e.log.Info("No operations in %s\n", p.Name())
 			continue
 		}
 
@@ -103,10 +110,6 @@ func listFiles(dir, pattern string) ([]os.FileInfo, error) {
 	return filteredFiles, nil
 }
 
-func indexHandler(c *vidar.Context) {
-	c.Text(200, "HELLO")
-}
-
 func notFoundHandler(c *vidar.Context) {
 	c.Error(constant.NotFoundError)
 }
@@ -127,34 +130,59 @@ func handlerWrapper(cf compFunc) vidar.ContextUserFunc {
 	}
 }
 
-func (e *essosd) Run(args ...string) {
+func (e *essosd) addHandler() {
+
+}
+
+func (e *essosd) runServer(args ...string) error {
 	commonHandler := vidar.NewChain()
 	v := vidar.New()
 
 	commonHandler.Append(middlewares.LoggingHandler)
 	commonHandler.Append(middlewares.RecoverHandler)
 
-	v.Router.Add("GET", "/", commonHandler.Use(indexHandler))
 	// v.Router.Add("POST", "/dns/create", commonHandler.Use(dnsCreateHandler))
 	v.Router.NotFound = commonHandler.Use(notFoundHandler)
 
 	if err := v.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		return err
 	}
+
+	return nil
 }
 
 func main() {
-
-	ParseConfig()
-
+	//TODO: To be refactor
 	e := NewEssosd()
 
-	e.loadPlugins()
-
-	if err := e.Run(os.Args[1:]...); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	tc, err := cmd.ParseConfig(configPath)
+	if err != nil {
+		e.log.Error(err)
 	}
+
+	e.log.Info("Configs: %+v\n", tc)
+
+	if err := e.loadPlugins(tc.LibraryPath, tc.Library); err != nil {
+		e.log.Error(err)
+	}
+
+	/*
+		if err := e.loadRPC(tc.RPC); err != nil {
+			e.log.Error(error)
+		}
+	*/
+
+	/*
+		if err := e.addHandler(); err != nil {
+			log.Error(err)
+		}
+
+		if err := e.runServer(os.Args[1:]...); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	*/
 }
 
 func NewEssosd() *essosd {
